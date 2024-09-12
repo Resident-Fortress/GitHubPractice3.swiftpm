0130e5e11a39f41c58a8bf4395d75e3f30e03adf
ME <78168583+JSmskd@users.noreply.github.com>
2024-09-12 11:17:43 -0500

    Not an ammend

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tasks:Array = ["Mow the lawn", "Work on OpenShift", "Finish Homework"]
+    @State var tasks:Array<Task> = [Task(nameOfTask:"Mow the lawn"), Task(nameOfTask:"Work on OpenShift"), Task(nameOfTask:"Finish Homework")]
     var body: some View {
         List(tasks, id: \.self){ tasks in
-                Text("\(tasks)")
+            Text("\(tasks.nameOfTask)")
             }
            
     }
